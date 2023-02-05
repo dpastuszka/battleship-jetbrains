808f4d19089c66713e351410fa0473a4b798adef
dpastuszka <48692793+dpastuszka@users.noreply.github.com>
2023-02-05 19:39:23 +0100

    test for Utilitites class method parseShipPlacementCoordinates

--- src/main/java/main/Utilities.java
+++ src/main/java/main/Utilities.java
@@ -12,52 +12,52 @@ public class Utilities {
         boolean gotRightCoordinates = false;
         int[][]  coordinates = new int[2][2];
 
-        try {
         while (!gotRightCoordinates) {
-            String inputString = input.nextLine().trim().toUpperCase();
-            String[] split = inputString.split("\\s+");
-            int pointAX = Row.valueOf(split[0].substring(0, 1)).getRowNumber();
-            int pointAY = Integer.parseInt(split[0].substring(1)) - 1;
-            int pointBX = Row.valueOf(split[1].substring(0, 1)).getRowNumber();
-            int pointBY = Integer.parseInt(split[1].substring(1)) - 1;
+            try {
+                String inputString = input.nextLine().trim().toUpperCase();
+                String[] split = inputString.split("\\s+");
+                int pointAX = Row.valueOf(split[0].substring(0, 1)).getRowNumber();
+                int pointAY = Integer.parseInt(split[0].substring(1)) - 1;
+                int pointBX = Row.valueOf(split[1].substring(0, 1)).getRowNumber();
+                int pointBY = Integer.parseInt(split[1].substring(1)) - 1;
 
-            if (pointAX == pointBX) {
-                int startY = Math.min(pointAY, pointBY);
-                int endY = Math.max(pointAY, pointBY);
-                int length = endY - startY;
-                if (shipLength == length + 1) {
-                    coordinates[0][0] = pointAX;
-                    coordinates[0][1] = startY;
-                    coordinates[1][0] = pointBX;
-                    coordinates[1][1] = endY;
-                    gotRightCoordinates = true;
+                if (pointAX == pointBX) {
+                    int startY = Math.min(pointAY, pointBY);
+                    int endY = Math.max(pointAY, pointBY);
+                    int length = endY - startY;
+                    if (shipLength == length + 1) {
+                        coordinates[0][0] = pointAX;
+                        coordinates[0][1] = startY;
+                        coordinates[1][0] = pointBX;
+                        coordinates[1][1] = endY;
+                        gotRightCoordinates = true;
+                    }
+                    else {
+                        throw new IllegalArgumentException("Error! Wrong ship length, try again:");
+                    }
+                }
+                else if (pointAY == pointBY) {
+                    int startX = Math.min(pointAX, pointBX);
+                    int endX = Math.max(pointAX, pointBX);
+                    int length = endX - startX;
+                    if (shipLength == length + 1) {
+                        coordinates[0][0] = startX;
+                        coordinates[0][1] = pointAY;
+                        coordinates[1][0] = endX;
+                        coordinates[1][1] = pointBY;
+                        gotRightCoordinates = true;
+                    }
+                    else {
+                        throw new IllegalArgumentException("Error! Wrong ship length, try again:");
+                    }
                 }
                 else {
-                    System.out.println("Error! Wrong ship length, try again:");
+                    throw new IllegalArgumentException("Error! Ship have to be vertical or horizontal");
                 }
-            }
-            else if (pointAY == pointBY) {
-                int startX = Math.min(pointAX, pointBX);
-                int endX = Math.max(pointAX, pointBX);
-                int length = endX - startX;
-                if (shipLength == length + 1) {
-                    coordinates[0][0] = startX;
-                    coordinates[0][1] = pointAY;
-                    coordinates[1][0] = endX;
-                    coordinates[1][1] = pointBY;
-                    gotRightCoordinates = true;
-                }
-                else {
-                    System.out.println("Error! Wrong ship length, try again:");
-                }
-            }
-            else {
-                System.out.println("Error! Ship have to be vertical or horizontal");
-            }
 
-        }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error! Incorrect coordinates, try again:");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
 
         return coordinates;
--- src/test/java/main/UtilitiesTest.java
+++ src/test/java/main/UtilitiesTest.java
@@ -6,7 +6,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,7 +15,7 @@ public class UtilitiesTest {
     Scanner mockScanner = mock(Scanner.class);
 
     @Test
-    public void testParseShipPlacementCoordinatesCase1() {
+    public void shouldParseCorrectCoordsInHorizontalRight() {
 
         when(mockScanner.nextLine()).thenReturn("A1 B1");
         Utilities.input = mockScanner;
@@ -28,26 +28,84 @@ public class UtilitiesTest {
     }
 
     @Test
-    public void testParseShipPlacementCoordinatesCase2() {
-        when(mockScanner.nextLine()).thenReturn("A1 C1");
+    public void shouldParseCorrectCoordsInHorizontalLeft() {
+
+        when(mockScanner.nextLine()).thenReturn("J10 J8");
         Utilities.input = mockScanner;
 
         int[][] coordinates = Utilities.parseShipPlacementCoordinates(3);
-        int[][] expectedCoordinates = {{0, 0}, {2, 0}};
+        int[][] expectedCoordinates = {{9, 7}, {9, 9}};
         assertArrayEquals(expectedCoordinates, coordinates);
 
         verify(mockScanner, times(1)).nextLine();
     }
 
     @Test
-    public void testParseShipPlacementCoordinatesCase3() {  //todo błąd w teście/ klasie
-        when(mockScanner.nextLine()).thenReturn("D2 E7");
+    public void shouldParseCorrectCoordsVerticalDownward() {
+        when(mockScanner.nextLine()).thenReturn("D3 H3");
         Utilities.input = mockScanner;
 
-        int[][] coordinates = Utilities.parseShipPlacementCoordinates(2);
-        int[][] expectedCoordinates = {{3, 1}, {4, 6}};
+        int[][] coordinates = Utilities.parseShipPlacementCoordinates(5);
+        int[][] expectedCoordinates = {{3, 2}, {7, 2}};
         assertArrayEquals(expectedCoordinates, coordinates);
 
         verify(mockScanner, times(1)).nextLine();
     }
+
+    @Test
+    public void shouldParseCorrectCoordsVerticalUpward() {
+        when(mockScanner.nextLine()).thenReturn("G7 E7");
+        Utilities.input = mockScanner;
+
+        int[][] coordinates = Utilities.parseShipPlacementCoordinates(3);
+        int[][] expectedCoordinates = {{4, 6}, {6, 6}};
+        assertArrayEquals(expectedCoordinates, coordinates);
+
+        verify(mockScanner, times(1)).nextLine();
+    }
+//
+//    @Test
+//    public void shouldThrowExceptionWhenTooChoordsShort() {
+//        when(mockScanner.nextLine()).thenReturn("G7 E7");
+//        Utilities.input = mockScanner;
+//        try {
+//            int[][] coordinates = Utilities.parseShipPlacementCoordinates(4);
+//            fail("Expected an IllegalArgumentException to be thrown");
+//        } catch (IllegalArgumentException e) {
+//            assertEquals("Error! Ship have to be vertical or horizontal", e.getMessage());
+//        }
+//
+//        verify(mockScanner, times(1)).nextLine();
+//    }
+//
+//    @Test
+//    public void shouldThrowExceptionWhenCoordsTooLong() {
+//        when(mockScanner.nextLine()).thenReturn("G7 E7");
+//        Utilities.input = mockScanner;
+//        try {
+//            int[][] coordinates = Utilities.parseShipPlacementCoordinates(2);
+//            fail("Expected an IllegalArgumentException to be thrown");
+//        } catch (IllegalArgumentException e) {
+//            assertEquals("Error! Ship have to be vertical or horizontal", e.getMessage());
+//        }
+//
+//        verify(mockScanner, times(1)).nextLine();
+//    }
+//
+
+//    @Test
+//    public void shouldThrowIllegalArgumentExceptionWhenCoordinatesNotInLine() {
+//        when(mockScanner.nextLine()).thenReturn("D2 E7");
+//        Utilities.input = mockScanner;
+//        try {
+//            int[][] coordinates = Utilities.parseShipPlacementCoordinates(5);
+//            fail("Expected an IllegalArgumentException to be thrown");
+//        } catch (IllegalArgumentException e) {
+//            assertEquals("Error! Ship have to be vertical or horizontal", e.getMessage());
+//        }
+//
+//        verify(mockScanner, times(1)).nextLine();
+//    }
+
+    //todo test for coordinates out of map
 }
